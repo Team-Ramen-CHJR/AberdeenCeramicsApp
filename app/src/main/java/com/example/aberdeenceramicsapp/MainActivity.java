c50815afac9de2d311d0979627c2a032ba4a6ace
Jake <j.milne27@rgu.ac.uk>
2023-11-03 14:24:21 +0000

    example code for firbase

--- app/src/main/java/com/example/aberdeenceramicsapp/MainActivity.java
+++ app/src/main/java/com/example/aberdeenceramicsapp/MainActivity.java
@@ -10,5 +10,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        URL url = new URL("https://teamramen-c8713.eu3.firebasedatabase.app/") // this should be the url but I might be wrong. it will need to be passed when using getInstance
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+DatabaseReference myRef = database.getReference("message");
+        // Read from the database
+myRef.addValueEventListener(new ValueEventListener() {
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        // This method is called once with the initial value and again
+        // whenever data at this location is updated.
+        String value = dataSnapshot.getValue(String.class);
+        Log.d(TAG, "Value is: " + value);
+    }
+
+    @Override
+    public void onCancelled(DatabaseError error) {
+        // Failed to read value
+        Log.w(TAG, "Failed to read value.", error.toException());
+    }
+});
+         */
     }
 }
