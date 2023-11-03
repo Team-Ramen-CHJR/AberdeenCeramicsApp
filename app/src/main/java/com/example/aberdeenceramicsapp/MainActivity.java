5a16392b5cfc8189caa6ef3d6f7f7a4722f3c34a
HamsterJesus <2104302@rgu.ac.uk>
2023-11-03 15:23:39 +0000

    nav start

--- app/src/main/java/com/example/aberdeenceramicsapp/MainActivity.java
+++ app/src/main/java/com/example/aberdeenceramicsapp/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.aberdeenceramicsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener{
+
+    //define the nav bar
+    BottomNavigationView bottomNavbarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavbarView = findViewById(R.id.navbarView);
+        bottomNavbarView.setOnItemSelectedListener(this);
+        bottomNavbarView.setSelectedItemId(R.id.profile_nb);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
